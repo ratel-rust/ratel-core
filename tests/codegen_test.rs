b4c119a130b7837385028408570b505967b10b79
Jan Schulte <hello@unexpected-co.de>
2016-05-01 15:51:58 +0200

    add tests for more literal values

--- tests/codegen_test.rs
+++ tests/codegen_test.rs
@@ -23,3 +23,23 @@ fn convert_const_str_to_var() {
 fn convert_var_str_to_var() {
     assert_eq!(output_program("var foo = \"bar\";"), Ok("var foo = \"bar\";".into()));
 }
+
+#[test]
+fn convert_const_number_to_var() {
+    assert_eq!(output_program("const foo = 435;"), Ok("var foo = 435;".into()));
+}
+
+#[test]
+fn convert_const_true_to_var() {
+    assert_eq!(output_program("const foo = true;"), Ok("var foo = true;".into()));
+}
+
+#[test]
+fn convert_const_false_to_var() {
+    assert_eq!(output_program("const foo = false;"), Ok("var foo = false;".into()));
+}
+
+#[test]
+fn convert_const_null_to_var() {
+    assert_eq!(output_program("const foo = null;"), Ok("var foo = null;".into()));
+}
