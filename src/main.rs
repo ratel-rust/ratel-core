022c861d778d5a12b5c971f1cf3accc206434667
Jan Schulte <hello@unexpected-co.de>
2016-05-01 23:49:36 +0200

    Write output code into new file

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ pub mod tokenizer;
 pub mod parser;
 pub mod grammar;
 pub mod transformer;
+pub mod codegen;
 extern crate docopt;
 extern crate rustc_serialize;
 
@@ -33,6 +34,14 @@ fn read_file(path: &str) -> Result<String, Error> {
     let mut s = String::new();
     match f.read_to_string(&mut s) {
         Ok(_) => Ok(s),
+        Err(err) => Err(err)
+    }
+}
+
+fn write_file(filename: &str, program: String) -> Result<(), Error> {
+    let mut f = try!(File::create(filename));
+    match f.write_all(&program.into_bytes()[..]) {
+        Ok(_) => Ok(()),
         Err(err) => Err(err)
     }
 }
@@ -64,5 +73,21 @@ fn main() {
     let ast = parser::parse(file);
 
     let transformed_ast = transformer::traverse(ast);
-    println!("{:?}", transformed_ast);
+    let program = match codegen::generate_code(transformed_ast) {
+        Ok(program) => program,
+        Err(errs) => {
+            for err in errs {
+                println!("ERR {}", err);
+            }
+            process::exit(1);
+        }
+    };
+
+    match write_file("out.js", program) {
+        Ok(()) => {},
+        Err(err) => {
+            println!("ERR Writing out.js {}", err);
+            process::exit(1);
+        }
+    }
 }
