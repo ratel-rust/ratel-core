e64ffbf9f1b42d90943084952765cf2062dc7720
Maciej Hirsz <maciej.hirsz@gmail.com>
2016-10-01 16:13:01 +0200

    Fast labels

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,6 +1,5 @@
 use std::str;
-use std::str::Bytes;
-use std::iter::{ Peekable, Iterator };
+use std::iter::Iterator;
 use lexicon::Token;
 use lexicon::Token::*;
 use lexicon::ReservedKind::*;
@@ -31,10 +30,13 @@ macro_rules! match_descend {
     ( $tokenizer:ident $matched:ident { $(
         $secondary:pat => $tok:ident $cascade:tt
     )* } ) => ({
-        match $tokenizer.source.peek() {
+        if $tokenizer.is_eof() {
+            return Some(Operator($matched));
+        }
+        match $tokenizer.read_byte() {
             $(
-                Some(&$secondary) => {
-                    $tokenizer.source.next();
+                $secondary => {
+                    $tokenizer.bump();
                     match_descend!( $tokenizer $tok $cascade )
                 },
             )*
@@ -52,23 +54,79 @@ macro_rules! match_descend {
 
 
 pub struct Tokenizer<'a> {
-    source: Peekable<Bytes<'a>>,
+    // Helper buffer for parsing strings that can't be just memcopied from
+    // the original source (escaped characters)
     buffer: Vec<u8>,
+
+    // String slice to parse
+    source: &'a str,
+
+    // Byte pointer to the slice above
+    byte_ptr: *const u8,
+
+    // Current index
+    index: usize,
+
+    // Lenght of the source
+    length: usize,
+
+    // Auxiliary index informing when last token begun
+    pub token_start: usize,
 }
 
 impl<'a> Tokenizer<'a> {
     pub fn new(source: &'a str) -> Self {
         Tokenizer {
-            source: source.bytes().peekable(),
-            buffer: Vec::with_capacity(128),
+            buffer: Vec::with_capacity(30),
+            source: source,
+            byte_ptr: source.as_ptr(),
+            index: 0,
+            token_start: 0,
+            length: source.len(),
         }
+    }
+
+    // Check if we are at the end of the source.
+    #[inline]
+    fn is_eof(&mut self) -> bool {
+        self.index == self.length
+    }
+
+    // Read a byte from the source. Note that this does not increment
+    // the index. In few cases (all of them related to number parsing)
+    // we want to peek at the byte before doing anything. This will,
+    // very very rarely, lead to a situation where the same byte is read
+    // twice, but since this operation is using a raw pointer, the cost
+    // is virtually irrelevant.
+    #[inline]
+    fn read_byte(&self) -> u8 {
+        unsafe { *self.byte_ptr.offset(self.index as isize) }
+    }
+
+    // Manually increment the index. Calling `read_byte` and then `bump`
+    // is equivalent to consuming a byte on an iterator.
+    #[inline]
+    fn bump(&mut self) {
+        self.index += 1;
+    }
+
+    #[inline]
+    fn expect_byte(&mut self) -> u8 {
+        if self.is_eof() {
+            panic!("Unexpected end of source");
+        }
+
+        let ch = self.read_byte();
+        self.bump();
+        ch
     }
 
     fn read_string(&mut self, first: u8) -> String {
         self.buffer.clear();
         let mut escape = false;
 
-        while let Some(ch) = self.source.next() {
+        while !self.is_eof() {
+            let ch = self.expect_byte();
             if ch == first && escape == false {
                 break;
             }
@@ -94,14 +152,15 @@ impl<'a> Tokenizer<'a> {
     fn read_binary(&mut self) -> LiteralValue {
         let mut value = 0;
 
-        while let Some(&peek) = self.source.peek() {
+        while !self.is_eof() {
+            let peek = self.read_byte();
             match peek {
                 b'0' | b'1' => {
                     value <<= 1;
                     if peek == b'1' {
                         value += 1;
                     }
-                    self.source.next();
+                    self.bump();
                 },
                 _ => return LiteralInteger(value),
             }
@@ -113,7 +172,9 @@ impl<'a> Tokenizer<'a> {
     fn read_octal(&mut self) -> LiteralValue {
         let mut value = 0;
 
-        while let Some(&peek) = self.source.peek() {
+        while !self.is_eof() {
+            let peek = self.read_byte();
+
             let digit = match peek {
                 b'0' => 0,
                 b'1' => 1,
@@ -130,7 +191,7 @@ impl<'a> Tokenizer<'a> {
             } else {
                 value <<= 3;
                 value += digit;
-                self.source.next();
+                self.bump();
             }
         }
 
@@ -140,7 +201,8 @@ impl<'a> Tokenizer<'a> {
     fn read_hexadec(&mut self) -> LiteralValue {
         let mut value = 0;
 
-        while let Some(&peek) = self.source.peek() {
+        while !self.is_eof() {
+            let peek = self.read_byte();
             let digit = match peek {
                 b'0'        => 0,
                 b'1'        => 1,
@@ -165,7 +227,7 @@ impl<'a> Tokenizer<'a> {
             } else {
                 value <<= 4;
                 value += digit;
-                self.source.next();
+                self.bump();
             }
         }
 
@@ -180,31 +242,37 @@ impl<'a> Tokenizer<'a> {
         if first == b'.' {
             period = true;
         } else if first == b'0' {
-            if let Some(&peek) = self.source.peek() {
-                if peek == b'b' {
-                    self.source.next();
-                    return self.read_binary();
-                } else if peek == b'o' {
-                    self.source.next();
-                    return self.read_octal();
-                } else if peek == b'x' {
-                    self.source.next();
-                    return self.read_hexadec();
+            if !self.is_eof() {
+                match self.read_byte() {
+                    b'b' => {
+                        self.bump();
+                        return self.read_binary();
+                    },
+                    b'o' => {
+                        self.bump();
+                        return self.read_octal();
+                    },
+                    b'x' => {
+                        self.bump();
+                        return self.read_hexadec();
+                    },
+                    _ => {}
                 }
             }
         }
 
-        while let Some(&ch) = self.source.peek() {
+        while !self.is_eof() {
+            let ch = self.read_byte();
             match ch {
                 b'0'...b'9' => {
                     self.buffer.push(ch);
-                    self.source.next();
+                    self.bump();
                 },
                 b'.' => {
                     if !period {
                         period = true;
                         self.buffer.push(ch);
-                        self.source.next();
+                        self.bump();
                     } else {
                         return LiteralValue::float_from_string(&self.buffer);
                     }
@@ -217,95 +285,86 @@ impl<'a> Tokenizer<'a> {
     }
 
     fn read_comment(&mut self) {
-        while let Some(&ch) = self.source.peek() {
-            if ch == b'\n' {
+        while !self.is_eof() {
+            if self.read_byte() == b'\n' {
                 return;
             }
-            self.source.next();
+            self.bump();
         }
     }
 
     fn read_block_comment(&mut self) {
-        let mut asterisk = false;
-
-        while let Some(ch) = self.source.next() {
-            if ch == b'/' && asterisk {
+        loop {
+            if self.expect_byte() == b'*' && self.expect_byte() == b'/' {
                 return;
-            }
-            if ch == b'*' {
-                asterisk = true;
             }
         }
     }
 
-    fn read_label(&mut self, first: u8) -> Token {
-        self.buffer.clear();
-        self.buffer.push(first);
+    fn read_label(&mut self) -> Token {
+        let start = self.index - 1;
 
-        while let Some(&ch) = self.source.peek() {
-            match ch {
+        while !self.is_eof() {
+            match self.read_byte() {
                 b'a'...b'z' |
                 b'A'...b'Z' |
                 b'0'...b'9' |
-                b'_' | b'$' => {
-                    self.buffer.push(ch);
-                    self.source.next();
-                }
+                b'_' | b'$' => self.bump(),
                 _ => break
             }
         }
 
-        let slice: &[u8] = &self.buffer;
+        let slice = &self.source[start..self.index];
 
         match slice {
-            b"new"        => Operator(New),
-            b"typeof"     => Operator(Typeof),
-            b"delete"     => Operator(Delete),
-            b"void"       => Operator(Void),
-            b"in"         => Operator(In),
-            b"instanceof" => Operator(Instanceof),
-            b"var"        => Declaration(Var),
-            b"let"        => Declaration(Let),
-            b"const"      => Declaration(Const),
-            b"break"      => Break,
-            b"do"         => Do,
-            b"case"       => Case,
-            b"else"       => Else,
-            b"catch"      => Catch,
-            b"export"     => Export,
-            b"class"      => Class,
-            b"extends"    => Extends,
-            b"return"     => Return,
-            b"while"      => While,
-            b"finally"    => Finally,
-            b"super"      => Super,
-            b"with"       => With,
-            b"continue"   => Continue,
-            b"for"        => For,
-            b"switch"     => Switch,
-            b"yield"      => Yield,
-            b"debugger"   => Debugger,
-            b"function"   => Function,
-            b"this"       => This,
-            b"default"    => Default,
-            b"if"         => If,
-            b"throw"      => Throw,
-            b"import"     => Import,
-            b"try"        => Try,
-            b"await"      => Await,
-            b"static"     => Static,
-            b"true"       => Literal(LiteralTrue),
-            b"false"      => Literal(LiteralFalse),
-            b"undefined"  => Literal(LiteralUndefined),
-            b"null"       => Literal(LiteralNull),
-            b"enum"       => Reserved(Enum),
-            b"implements" => Reserved(Implements),
-            b"package"    => Reserved(Package),
-            b"protected"  => Reserved(Protected),
-            b"interface"  => Reserved(Interface),
-            b"private"    => Reserved(Private),
-            b"public"     => Reserved(Public),
-            _             => Identifier(unsafe { str::from_utf8_unchecked(slice) }.to_owned()),
+            "new"        => Operator(New),
+            "typeof"     => Operator(Typeof),
+            "delete"     => Operator(Delete),
+            "void"       => Operator(Void),
+            "in"         => Operator(In),
+            "instanceof" => Operator(Instanceof),
+            "var"        => Declaration(Var),
+            "let"        => Declaration(Let),
+            "const"      => Declaration(Const),
+            "break"      => Break,
+            "do"         => Do,
+            "case"       => Case,
+            "else"       => Else,
+            "catch"      => Catch,
+            "export"     => Export,
+            "class"      => Class,
+            "extends"    => Extends,
+            "return"     => Return,
+            "while"      => While,
+            "finally"    => Finally,
+            "super"      => Super,
+            "with"       => With,
+            "continue"   => Continue,
+            "for"        => For,
+            "switch"     => Switch,
+            "yield"      => Yield,
+            "debugger"   => Debugger,
+            "function"   => Function,
+            "this"       => This,
+            "default"    => Default,
+            "if"         => If,
+            "throw"      => Throw,
+            "import"     => Import,
+            "try"        => Try,
+            "await"      => Await,
+            "static"     => Static,
+            "true"       => Literal(LiteralTrue),
+            "false"      => Literal(LiteralFalse),
+            "undefined"  => Literal(LiteralUndefined),
+            "null"       => Literal(LiteralNull),
+            "enum"       => Reserved(Enum),
+            "implements" => Reserved(Implements),
+            "package"    => Reserved(Package),
+            "protected"  => Reserved(Protected),
+            "interface"  => Reserved(Interface),
+            "private"    => Reserved(Private),
+            "public"     => Reserved(Public),
+            _            => Identifier(slice.to_owned()),
         }
     }
 }
@@ -314,7 +373,10 @@ impl<'a> Iterator for Tokenizer<'a> {
     type Item = Token;
 
     fn next(&mut self) -> Option<Token> {
-        'lex: while let Some(ch) = self.source.next() {
+        while !self.is_eof() {
+            self.token_start = self.index;
+
+            let ch = self.expect_byte();
 
             match_operators! { self ch
                 b'=' => Assign {
@@ -390,56 +452,58 @@ impl<'a> Iterator for Tokenizer<'a> {
                     Literal(LiteralString( self.read_string(ch) ))
                 },
                 b'/' => {
-                    match self.source.peek() {
-                        Some(&b'/') => {
-                            self.source.next();
-                            self.read_comment();
-                            continue 'lex
-                        },
-                        Some(&b'*') => {
-                            self.source.next();
-                            self.read_block_comment();
-                            continue 'lex
-                        },
-                        Some(&b'=') => {
-                            self.source.next();
-                            Operator(DivideAssign)
-                        },
-                        _ => Operator(Division)
+                    if self.is_eof() {
+                        Operator(Division)
+                    } else {
+                        match self.read_byte() {
+                            b'/' => {
+                                self.bump();
+                                self.read_comment();
+                                continue;
+                            },
+                            b'*' => {
+                                self.bump();
+                                self.read_block_comment();
+                                continue;
+                            },
+                            b'=' => {
+                                self.bump();
+                                Operator(DivideAssign)
+                            },
+                            _ => Operator(Division)
+                        }
                     }
                 },
                 b'.' => {
-                    match self.source.peek() {
-                        Some(&b'0'...b'9') => {
-                            Literal(self.read_number(b'.'))
-                        },
-                        Some(&b'.') => {
-                            self.source.next();
-                            match self.source.next() {
-                                Some(b'.') => Operator(Spread),
-                                ch        => {
-                                    panic!("Invalid character `{:?}`", ch);
+                    if self.is_eof() {
+                        Operator(Accessor)
+                    } else {
+                        match self.read_byte() {
+                            b'0'...b'9' => {
+                                Literal(self.read_number(b'.'))
+                            },
+                            b'.' => {
+                                self.bump();
+                                match self.expect_byte() {
+                                    b'.' => Operator(Spread),
+                                    ch   => {
+                                        panic!("Invalid character `{:?}`", ch);
+                                    }
                                 }
-                            }
-                        },
-                        _ => Operator(Accessor)
+                            },
+                            _ => Operator(Accessor)
+                        }
                     }
                 },
                 b'0'...b'9' => Literal(self.read_number(ch)),
+                b'a'...b'z' |
+                b'A'...b'Z' |
+                b'$' | b'_' => self.read_label(),
+
+                b' ' | b'\t' => continue,
+
                 _ => {
-                    match ch {
-                        b'a'...b'z' |
-                        b'A'...b'Z' |
-                        b'$' | b'_' => {
-                            self.read_label(ch)
-                        },
-
-                        b' ' | b'\t' => continue 'lex,
-
-                        _ => {
-                            panic!("Invalid character `{:?}`", ch);
-                        }
-                    }
+                    panic!("Invalid character `{:?}`", ch);
                 }
             });
         }
