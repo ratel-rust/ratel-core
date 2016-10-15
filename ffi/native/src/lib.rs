1e096ae484a927ada4a028cbfb8ba6da156410a0
Maciej Hirsz <maciej.hirsz@gmail.com>
2016-10-15 20:00:33 +0200

    Fixed error handling.

--- ffi/native/src/lib.rs
+++ ffi/native/src/lib.rs
@@ -7,38 +7,57 @@ use neon::js::{JsString, Variant};
 use ratel::{parser, transformer, codegen};
 
 fn transform(call: Call) -> JsResult<JsString> {
-  let scope = call.scope;
+    let scope = call.scope;
 
-  let source = match call.arguments.get(scope, 0).unwrap().variant() {
-      Variant::String(handle) => handle.value() + "\n",
-      _                       => panic!("First argument must be a string"),
-  };
+    let source = match call.arguments.get(scope, 0).unwrap().variant() {
+        Variant::String(handle) => handle.value() + "\n",
+        _                       => panic!("First argument must be a string"),
+    };
 
-  let minify = match call.arguments.get(scope, 1).unwrap().variant() {
-      Variant::Boolean(handle) => handle.value(),
-      _                        => false,
-  };
+    let minify = match call.arguments.get(scope, 1).unwrap().variant() {
+        Variant::Boolean(handle) => handle.value(),
+        _                        => false,
+    };
 
-  let mut ast = parser::parse(source);
-  transformer::transform(&mut ast, transformer::Settings::target_es5());
-  let out = codegen::generate_code(ast, minify);
-  Ok(JsString::new(scope, &out).unwrap())
+    let mut ast = match parser::parse(source) {
+        Err(error) => {
+            println!("{}", error);
+
+            // TODO: return Ok(JsNull) or some such
+            ::std::process::exit(1);
+        },
+        Ok(ast) => ast,
+    };
+
+    transformer::transform(&mut ast, transformer::Settings::target_es5());
+    let out = codegen::generate_code(ast, minify);
+    Ok(JsString::new(scope, &out).unwrap())
 }
 
 fn parse(call: Call) -> JsResult<JsString> {
-  let scope = call.scope;
-  let source = match call.arguments.get(scope, 0).unwrap().variant() {
-      Variant::String(handle) => handle.value(),
-      _                       => panic!("First argument must be a string"),
-  };
+    let scope = call.scope;
+    let source = match call.arguments.get(scope, 0).unwrap().variant() {
+        Variant::String(handle) => handle.value(),
+        _                       => panic!("First argument must be a string"),
+    };
 
-  let ast = parser::parse(source);
-  let out = format!("{:#?}", ast.body);
-  Ok(JsString::new(scope, &out).unwrap())
+    let ast = match parser::parse(source) {
+        Err(error) => {
+            println!("{}", error);
+
+            // TODO: return Ok(JsNull) or some such
+            ::std::process::exit(1);
+        },
+        Ok(ast) => ast,
+    };
+
+    let out = format!("{:#?}", ast.body);
+
+    Ok(JsString::new(scope, &out).unwrap())
 }
 
 register_module!(m, {
-  try!(m.export("transform", transform));
-  try!(m.export("parse", parse));
-  Ok(())
+    try!(m.export("transform", transform));
+    try!(m.export("parse", parse));
+    Ok(())
 });
