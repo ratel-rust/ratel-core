970010ef232946d478354cfe1569deb81c29507d
maciejhirsz <maciej.hirsz@gmail.com>
2017-02-09 09:57:10 +0100

    Modularize the parser

--- core/src/parser/mod.rs
+++ core/src/parser/mod.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+mod macros;
+
 use std::mem;
 
 use ast::{Program, Store, Statement, Expression, OperatorKind};
@@ -5,100 +8,6 @@ use error::{Error, ParseResult, Result};
 use tokenizer::Tokenizer;
 use lexicon::Token;
 use lexicon::Token::*;
-
-/// Peek on the next token. Return with an error if tokenizer fails.
-macro_rules! peek {
-    ($parser:ident) => {
-        match $parser.token {
-            Some(token) => token,
-
-            None => {
-                let token = try!($parser.tokenizer.get_token());
-
-                $parser.token = Some(token);
-
-                token
-            }
-        }
-    }
-}
-
-/// Get the next token. Return with an error if tokenizer fails.
-macro_rules! next {
-    ($parser:ident) => {
-        match $parser.token {
-            Some(token) => {
-                $parser.consume();
-
-                token
-            },
-            None => try!($parser.tokenizer.get_token())
-        }
-    }
-}
-
-/// If the next token matches `$p`, consume that token and execute `$eval`.
-macro_rules! allow {
-    ($parser:ident, $p:pat => $eval:expr) => {
-        match peek!($parser) {
-            $p => {
-                $parser.consume();
-                $eval;
-            },
-            _ => {}
-        }
-    }
-}
-
-/// Return an error if the next token doesn't match $p.
-macro_rules! expect {
-    ($parser:ident, $p:pat) => {
-        match next!($parser) {
-            $p => {},
-            _  => unexpected_token!($parser)
-        }
-    }
-}
-
-/// Expect the next token to be an Identifier, extracting the OwnedSlice
-/// out of it. Returns an error otherwise.
-macro_rules! expect_identifier {
-    ($parser:ident) => {
-        match next!($parser) {
-            Identifier(ident) => ident,
-            _                 => unexpected_token!($parser)
-        }
-    }
-}
-
-/// Expecta semicolon to terminate a statement. Will assume a semicolon
-/// following the ASI rules.
-macro_rules! expect_semicolon {
-    ($parser:ident) => {
-        // TODO: Tokenizer needs to flag when a new line character has been
-        //       consumed to satisfy all ASI rules
-        match peek!($parser) {
-            Semicolon     => $parser.consume(),
-
-            ParenClose    |
-            BraceClose    |
-            EndOfProgram  => {},
-
-            _             => {
-                if !$parser.tokenizer.asi() {
-                    unexpected_token!($parser)
-                }
-            }
-        }
-    }
-}
-
-/// Return an error for current token.
-macro_rules! unexpected_token {
-    ($parser:ident) => {
-        return Err($parser.tokenizer.invalid_token())
-    };
-}
 
 pub struct Parser<'src> {
     /// Tokenizer will produce tokens from the source
@@ -231,7 +140,7 @@ impl<'src> Parser<'src> {
     }
 
     #[inline]
-    pub fn parse(&mut self) -> Result<()> {
+    fn parse(&mut self) -> Result<()> {
         let mut statement = match next!(self) {
             EndOfProgram => return Ok(()),
             token        => try!(self.statement(token))
