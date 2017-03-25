a55ee70be8e48d8786c9a9560c52c884193ff077
Matthias Thoemmes <git@gmx.org>
2017-03-25 11:57:35 +0100

    Initial AstDebug for all node types

--- core/src/ast/debug.rs
+++ core/src/ast/debug.rs
@@ -1,5 +1,10 @@
 use ast::{Index, OptIndex, Item, Program};
+use ast::item::*;
 use ast::Item::*;
+use ast::Nodes;
+use ast::variable::*;
+use ast::operator::OperatorKind;
+use error::Error;
 
 use std::fmt::{Debug, Formatter, Result};
 
@@ -56,10 +61,352 @@ impl<'src> DebugGen<'src> {
     }
 }
 
+impl<'src> AstDebug<'src> for OperatorKind {
+    fn ast_fmt(&self, gen: &mut DebugGen<'src>, f: &mut Formatter) -> Result {
+        match *self {
+            OperatorKind::FatArrow           => f.write_str("FatArrow")?,
+            OperatorKind::New                => f.write_str("New")?,
+            OperatorKind::Increment          => f.write_str("Increment")?,
+            OperatorKind::Decrement          => f.write_str("Decrement")?,
+            OperatorKind::LogicalNot         => f.write_str("LogicalNot")?,
+            OperatorKind::BitwiseNot         => f.write_str("BitwiseNot")?,
+            OperatorKind::Typeof             => f.write_str("Typeof")?,
+            OperatorKind::Void               => f.write_str("Void")?,
+            OperatorKind::Delete             => f.write_str("Delete")?,
+            OperatorKind::Multiplication     => f.write_str("Multiplication")?,
+            OperatorKind::Division           => f.write_str("Division")?,
+            OperatorKind::Remainder          => f.write_str("Remainder")?,
+            OperatorKind::Exponent           => f.write_str("Exponent")?,
+            OperatorKind::Addition           => f.write_str("Addition")?,
+            OperatorKind::Substraction       => f.write_str("Substraction")?,
+            OperatorKind::BitShiftLeft       => f.write_str("BitShiftLeft")?,
+            OperatorKind::BitShiftRight      => f.write_str("BitShiftRight")?,
+            OperatorKind::UBitShiftRight     => f.write_str("UBitShiftRight")?,
+            OperatorKind::Lesser             => f.write_str("Lesser")?,
+            OperatorKind::LesserEquals       => f.write_str("LesserEquals")?,
+            OperatorKind::Greater            => f.write_str("Greater")?,
+            OperatorKind::GreaterEquals      => f.write_str("GreaterEquals")?,
+            OperatorKind::Instanceof         => f.write_str("Instanceof")?,
+            OperatorKind::In                 => f.write_str("In")?,
+            OperatorKind::StrictEquality     => f.write_str("StrictEquality")?,
+            OperatorKind::StrictInequality   => f.write_str("StrictInequality")?,
+            OperatorKind::Equality           => f.write_str("Equality")?,
+            OperatorKind::Inequality         => f.write_str("Inequality")?,
+            OperatorKind::BitwiseAnd         => f.write_str("BitwiseAnd")?,
+            OperatorKind::BitwiseXor         => f.write_str("BitwiseXor")?,
+            OperatorKind::BitwiseOr          => f.write_str("BitwiseOr")?,
+            OperatorKind::LogicalAnd         => f.write_str("LogicalAnd")?,
+            OperatorKind::LogicalOr          => f.write_str("LogicalOr")?,
+            OperatorKind::Conditional        => f.write_str("Conditional")?,
+            OperatorKind::Assign             => f.write_str("Assign")?,
+            OperatorKind::AddAssign          => f.write_str("AddAssign")?,
+            OperatorKind::SubstractAssign    => f.write_str("SubstractAssign")?,
+            OperatorKind::ExponentAssign     => f.write_str("ExponentAssign")?,
+            OperatorKind::MultiplyAssign     => f.write_str("MultiplyAssign")?,
+            OperatorKind::DivideAssign       => f.write_str("DivideAssign")?,
+            OperatorKind::RemainderAssign    => f.write_str("RemainderAssign")?,
+            OperatorKind::BSLAssign          => f.write_str("BSLAssign")?,
+            OperatorKind::BSRAssign          => f.write_str("BSRAssign")?,
+            OperatorKind::UBSRAssign         => f.write_str("UBSRAssign")?,
+            OperatorKind::BitAndAssign       => f.write_str("BitAndAssign")?,
+            OperatorKind::BitXorAssign       => f.write_str("BitXorAssign")?,
+            OperatorKind::BitOrAssign        => f.write_str("BitOrAssign")?,
+            OperatorKind::Spread             => f.write_str("Spread")?,
+        }
+        Ok(())
+    }
+}
+
+impl<'src> AstDebug<'src> for VariableDeclarationKind {
+    fn ast_fmt(&self, gen: &mut DebugGen<'src>, f: &mut Formatter) -> Result {
+        match *self {
+            VariableDeclarationKind::Var => f.write_str("Var")?,
+            VariableDeclarationKind::Let => f.write_str("Let")?,
+            VariableDeclarationKind::Const => f.write_str("Const")?,
+        }
+        Ok(())
+    }
+}
+
+impl<'src> AstDebug<'src> for Value<'src> {
+    fn ast_fmt(&self, gen: &mut DebugGen<'src>, f: &mut Formatter) -> Result {
+        match *self {
+            Value::Undefined => f.write_str("Undefined")?,
+            Value::Null => f.write_str("Null")?,
+            Value::True => f.write_str("True")?,
+            Value::False => f.write_str("False")?,
+            Value::Number(value) => {
+                f.write_str("Number(")?;
+                f.write_str(value)?;
+                f.write_str(")")?;
+            },
+            Value::Binary(value) => {
+                f.write_str("Binary(")?;
+                f.write_str(value.to_string().as_str())?;
+                f.write_str(")")?;
+            },
+            Value::String(value) => {
+                f.write_str("String(")?;
+                f.write_str(value)?;
+                f.write_str(")")?;
+            },
+            Value::RawQuasi(value) => {
+                f.write_str("RawQuasi(")?;
+                f.write_str(value)?;
+                f.write_str(")")?;
+            },
+            Value::RegEx(value) => {
+                f.write_str("RegEx(")?;
+                f.write_str(value)?;
+                f.write_str(")")?;
+            },
+        }
+        Ok(())
+    }
+}
+
 impl<'src> AstDebug<'src> for Item<'src> {
     fn ast_fmt(&self, gen: &mut DebugGen<'src>, f: &mut Formatter) -> Result {
         match *self {
-            _ => f.write_str("💀")?
+            Error(error) => {
+                f.write_str("Error(")?;
+                let value = match error {
+                    Error::UnexpectedEndOfProgram { .. } => "UnexpectedEndOfProgram",
+                    Error::UnexpectedToken { .. } => "UnexpectedToken",
+                };
+                f.write_str(value)?;
+                f.write_str(")")?;
+            },
+
+            // Identifiers
+            Identifier(ident) => f.write_str(ident.as_str())?,
+            This => f.write_str("This")?,
+
+            // Expressions
+            ValueExpr(value) => {
+                f.write_str("ValueExpr(")?;
+                value.ast_fmt(gen, f)?;
+                f.write_str(")")?;
+            },
+            ArrayExpr(index) => {
+                f.write_str("ArrayExpr(")?;
+                gen.write_from_optindex(index, f);
+                f.write_str(")")?;
+            },
+            SequenceExpr(index) => {
+                f.write_str("SequenceExpr(")?;
+                gen.write_from_index(index, f);
+                f.write_str(")")?;
+            },
+            MemberExpr { object, property } => {
+                f.write_str("MemberExpr { object: ")?;
+                gen.write_from_index(object, f);
+                f.write_str(", property: ")?;
+                gen.write_from_index(property, f);
+                f.write_str(" }")?;
+            },
+            CallExpr { callee, arguments } => {
+                f.write_str("CallExpr { callee: ")?;
+                gen.write_from_index(callee, f);
+                f.write_str(", arguments: ")?;
+                gen.write_from_optindex(arguments, f);
+                f.write_str(" }")?;
+            },
+            BinaryExpr { parenthesized, operator, left, right } => {
+                f.write_str("BinaryExpr { parenthesized: ")?;
+                f.write_str(if parenthesized { "true" } else { "false" })?;
+                f.write_str(", operator: ")?;
+                operator.ast_fmt(gen, f);
+                f.write_str(", left: ")?;
+                gen.write_from_index(left, f);
+                f.write_str(", right: ")?;
+                gen.write_from_index(right, f);
+                f.write_str(" }")?;
+            },
+            Prefix { operator, operand } => {
+                f.write_str("Prefix { operator: ")?;
+                operator.ast_fmt(gen, f);
+                f.write_str(", operand: ")?;
+                gen.write_from_index(operand, f);
+                f.write_str(" }")?;
+            },
+            PostfixExpr { operator, operand } => {
+                f.write_str("PostfixExpr { operator: ")?;
+                operator.ast_fmt(gen, f);
+                f.write_str(", operand: ")?;
+                gen.write_from_index(operand, f);
+                f.write_str(" }")?;
+            },
+            ConditionalExpr { test, consequent, alternate } => {
+                f.write_str("ConditionalExpr { test: ")?;
+                gen.write_from_index(test, f);
+                f.write_str(", consequent: ")?;
+                gen.write_from_index(consequent, f);
+                f.write_str(", alternate: ")?;
+                gen.write_from_index(alternate, f);
+                f.write_str(" }")?;
+            },
+            ArrowExpr { params, body } => {
+                f.write_str("ArrowExpr { params: ")?;
+                gen.write_from_optindex(params, f);
+                f.write_str(", body: ")?;
+                gen.write_from_optindex(body, f);
+                f.write_str(" }")?;
+            },
+            FunctionExpr { name, params, body } => {
+                f.write_str("FunctionExpr { name: ")?;
+                gen.write_from_optindex(name, f);
+                f.write_str(", params: ")?;
+                gen.write_from_optindex(params, f);
+                f.write_str(", body: ")?;
+                gen.write_from_optindex(body, f);
+                f.write_str(" }")?;
+            },
+            ObjectExpr { body } => {
+                f.write_str("ObjectExpr { body: ")?;
+                gen.write_from_optindex(body, f);
+                f.write_str(" }")?;
+            },
+            ClassExpr { name, extends, body } => {
+                f.write_str("ClassExpr { name: ")?;
+                gen.write_from_optindex(name, f);
+                f.write_str(", extends: ")?;
+                gen.write_from_optindex(extends, f);
+                f.write_str(", body: ")?;
+                gen.write_from_optindex(body, f);
+                f.write_str(" }")?;
+            },
+
+            // Object
+            ShorthandMember(ident) => {
+                f.write_str("ShorthandMember(")?;
+                f.write_str(ident.as_str())?;
+                f.write_str(")")?;
+            },
+            ObjectMember { key, value } => {
+                f.write_str("ObjectMember { key: ")?;
+                gen.write_from_index(key, f);
+                f.write_str(", value: ")?;
+                gen.write_from_index(value, f);
+                f.write_str(" }")?;
+            },
+
+            // Declaration
+            VariableDeclarator { name, value } => {
+                f.write_str("VariableDeclarator { name: ")?;
+                gen.write_from_index(name, f);
+                f.write_str(", value: ")?;
+                gen.write_from_optindex(value, f);
+                f.write_str(" }")?;
+            },
+
+            // Statements
+
+            EmptyStatement => {
+                f.write_str("EmptyStatement")?;
+            },
+            ExpressionStatement(index) => {
+                f.write_str("ExpressionStatement(")?;
+                gen.write_from_index(index, f);
+                f.write_str(")")?;
+            },
+            DeclarationStatement { kind, declarators } => {
+                f.write_str("DeclarationStatement { kind: ")?;
+                kind.ast_fmt(gen, f);
+                f.write_str(", declarators: ")?;
+                gen.write_from_index(declarators, f);
+                f.write_str(" }")?;
+            },
+            FunctionStatement { name, params, body } => {
+                f.write_str("FunctionStatement { name: ")?;
+                gen.write_from_index(name, f);
+                f.write_str(", params: ")?;
+                gen.write_from_optindex(params, f);
+                f.write_str(", body: ")?;
+                gen.write_from_optindex(body, f);
+                f.write_str(" }")?;
+            },
+            ReturnStatement { value } => {
+                f.write_str("ReturnStatement { value: ")?;
+                gen.write_from_optindex(value, f);
+                f.write_str(" }")?;
+            },
+            BreakStatement { label} => {
+                f.write_str("BreakStatement { label: ")?;
+                gen.write_from_optindex(label, f);
+                f.write_str(" }")?;
+            },
+            IfStatement { test, consequent, alternate } => {
+                f.write_str("IfStatement { test: ")?;
+                gen.write_from_index(test, f);
+                f.write_str(", consequent: ")?;
+                gen.write_from_index(consequent, f);
+                f.write_str(", alternate: ")?;
+                gen.write_from_optindex(alternate, f);
+                f.write_str(" }")?;
+            },
+            WhileStatement { test, body } => {
+                f.write_str("WhileStatement { test: ")?;
+                gen.write_from_index(test, f);
+                f.write_str(", body: ")?;
+                gen.write_from_index(body, f);
+                f.write_str(" }")?;
+            },
+            DoStatement { body, test } => {
+                f.write_str("DoStatement { body: ")?;
+                gen.write_from_index(body, f);
+                f.write_str(", test: ")?;
+                gen.write_from_index(test, f);
+                f.write_str(" }")?;
+            },
+            ForStatement { init, test, update, body } => {
+                f.write_str("ForStatement { init: ")?;
+                gen.write_from_optindex(init, f);
+                f.write_str(", test: ")?;
+                gen.write_from_optindex(test, f);
+                f.write_str(", update: ")?;
+                gen.write_from_optindex(update, f);
+                f.write_str(", body: ")?;
+                gen.write_from_index(body, f);
+                f.write_str(" }")?;
+            },
+            ForIn { left, right, body } => {
+                f.write_str("ForIn { left: ")?;
+                gen.write_from_index(left, f);
+                f.write_str(", right: ")?;
+                gen.write_from_index(right, f);
+                f.write_str(", body: ")?;
+                gen.write_from_index(body, f);
+                f.write_str("}")?;
+            },
+            ForOf { left, right, body } => {
+                f.write_str("ForOf { left: ")?;
+                gen.write_from_index(left, f);
+                f.write_str(", right: ")?;
+                gen.write_from_index(right, f);
+                f.write_str(", body: ")?;
+                gen.write_from_index(body, f);
+                f.write_str(" }")?;
+            },
+            ThrowStatement { value } => {
+                f.write_str("ThrowStatement { value: ")?;
+                gen.write_from_index(value, f);
+                f.write_str(" }")?;
+            },
+            TryStatement { body, error, handler } => {
+                f.write_str("TryStatement { body: ")?;
+                gen.write_from_optindex(body, f);
+                f.write_str(", error: ")?;
+                gen.write_from_index(error, f);
+                f.write_str(", handler: ")?;
+                gen.write_from_optindex(handler, f);
+                f.write_str(" }")?;
+            },
+            BlockStatement { body } => {
+                f.write_str("BlockStatement { body: ")?;
+                gen.write_from_optindex(body, f);
+                f.write_str(" }")?;
+            },
         }
 
         Ok(())
@@ -69,8 +416,8 @@ impl<'src> AstDebug<'src> for Item<'src> {
 impl<'src> Debug for Program<'src> {
     fn fmt(&self, f: &mut Formatter) -> Result {
         let mut gen = DebugGen::new(self);
-
-        gen.write_from_optindex(self.root, f)
+        gen.write_from_optindex(self.root, f);
+        Ok(())
     }
 }
 
