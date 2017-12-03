c2f3392d9b4cedd6e0a4586c795baf71a0148269
maciejhirsz <maciej.hirsz@gmail.com>
2017-12-03 09:46:40 +0100

    Wrap it up inside a workspace

--- test.sh
+++ test.sh
@@ -1,40 +1,10 @@
-echo "TESTING: ratel"
-echo "##############"
+echo ""
+echo ""
+echo "TESTING: ratel + crates"
+echo "#######################"
 echo ""
 
-cd ratel
 cargo test || exit
-cd ..
-
-echo ""
-echo ""
-echo "TESTING: ratel-codegen"
-echo "######################"
-echo ""
-
-cd ratel-codegen
-cargo test || exit
-cd ..
-
-echo ""
-echo ""
-echo "TESTING: ratel-visitor"
-echo "######################"
-echo ""
-
-cd ratel-visitor
-cargo test || exit
-cd ..
-
-echo ""
-echo ""
-echo "TESTING: ratel-transformer"
-echo "##########################"
-echo ""
-
-cd ratel-transformer
-cargo test || exit
-cd ..
 
 echo ""
 echo ""
